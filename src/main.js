4d7146f5a4e22547aeb2b5985f6dfbdad2ba021a
alvaro_rc13 <alvarorioscallejas09@gmail.com>
2025-11-24 09:16:41 +0100

    Subiendo estructura

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { router } from "./router";
+import { Navbar } from "./components/Navbar";
+import "./styles/style.css";
+document.querySelector("#app").innerHTML = 
+`
+${Navbar()}
+<main id="view"></main>
+`;
+router();
+window.addEventListener("hashchange", router);
